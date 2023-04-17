16a51ff3647277c1d45085ed2efba838e9b3ae35
aolvera8a <aolvera8a@griddynamics.com>
2023-04-17 17:16:30 -0600

    Initial commit

---
+++ src/main/java/gd/ao8a/edupltf/EducationalPlatformApplication.java
@@ -0,0 +1,13 @@
+package gd.ao8a.edupltf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationalPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gd/ao8a/edupltf/EducationalPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package gd.ao8a.edupltf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
